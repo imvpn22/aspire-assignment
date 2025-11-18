d321fae9e033e8335f2a0b6fa71db14ab132361a
Vipin Yadav <imvpn22@gmail.com>
2025-11-18 18:37:27 +0530

    fix breaking style

--- src/pages/CardsPage.tsx
+++ src/pages/CardsPage.tsx
@@ -2,7 +2,10 @@ import Cards from "../components/Cards";
 
 const CardsPage: React.FC = () => {
   return (
-    <div data-testid="cards-page">
+    <div
+      data-testid="cards-page"
+      className="flex flex-col flex-1 overflow-hidden"
+    >
       <Cards />
     </div>
   );
